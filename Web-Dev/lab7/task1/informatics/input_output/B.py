910099c920d6cf06d415d80f7a59728020a1e759
Talantttt <144251182+Talantttt@users.noreply.github.com>
2025-03-23 15:35:39 +0500

    Add files via upload

---
+++ Web-Dev/lab7/task1/informatics/input_output/B.py
@@ -0,0 +1,3 @@
+a = int(input())
+print("The","next", "number", "for", "the", "number", a, "is", str(a+1) + ".", sep = " ")
+print("The","previous", "number", "for", "the", "number", a, "is", str(a-1) + ".", sep = " ")
